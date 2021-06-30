e4be0bf94124e165702f77b1cfebaee34dedb922
FernandoPinage <luizfernandoluck@hotmail.com>
2021-06-30 17:43:50 -0400

    atualização

--- php/index.php
+++ php/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel='stylesheet' href="../css/index.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
     <title>index</title>
 </head>
 
@@ -15,12 +16,23 @@
 
     <form class="form-signin" method="POST">
         <div class="text-center">
-            <img id="logo" src="../img/logo.png" alt="" >
+            <img id="logo" src="../img/logo.png" alt="">
             <hr>
         </div>
-        <input type="text" class="form-control" name="valor" placeholder="E-mail" required="" autofocus="" />
-        <br>
-        <input type="password" class="form-control" name="password" placeholder="Senha:" required="" />
+
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Email</span>
+            </div>
+            <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+        </div>
+       
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">Senha</span>
+            </div>
+            <input type="password" class="form-control" placeholder="Username" aria-label="" aria-describedby="basic-addon1">
+        </div>
         <br>
         <div class="text-left" id="cadastro">
 
