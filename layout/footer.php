61b36a546fdc09181da6972d447ae506de6edc8b
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-02 16:36:53 -0400

    atualizar

--- layout/footer.php
+++ layout/footer.php
@@ -1,14 +1,17 @@
 <footer>
-<nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
-</nav>
 
-<script type="text/javascript" src="../js/mascaraTelefone.js"></script> <!-- mascara de telefone -->
-<script type="text/javascript" src="../js/mascaraCNPJ.js"></script> <!-- mascara de cnpj -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <!-- jquery CDN  -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    
+    <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark justify-content-center">
+        <p style="color:#fff;"> Desenvolvido por Progride ®</p>
+    </nav>
+
+    <script type="text/javascript" src="../js/mascaraTelefone.js"></script> <!-- mascara de telefone -->
+    <script type="text/javascript" src="../js/mascaraCNPJ.js"></script> <!-- mascara de cnpj -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <!-- jquery CDN  -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
 
 </footer>
-</html>
 
+</html>
